7d0f6ed30665dd5328d6a142434fcd6418763ce9
Diego Galeota <diego@thinkcraft.co.uk>
2020-02-15 20:22:49 +0000

    Refactor createStore

--- src/createStore.ts
+++ src/createStore.ts
@@ -3,18 +3,12 @@ import { wrapObservable } from "./core";
 import { useStore } from "./useStore";
 
 export function createStore<T extends Object>(target: T) {
-    let isFunctionComponent = false;
-
     try {
         useEffect(() => {}, []);
 
-        isFunctionComponent = true;
+        throw new Error(`Attempting to use ${createStore.name}() inside a function component. Please use ${useStore.name}() instead.`);
     }
     catch (err) {
-    }
-
-    if (isFunctionComponent) {
-        throw new Error(`Attempting to use ${createStore.name}() inside a function component. Please use ${useStore.name}() instead.`);
     }
 
     return wrapObservable(target);
